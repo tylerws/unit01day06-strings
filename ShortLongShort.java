e3f0a782a21fddbf9c622da951c8a364b0d1dff2
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-23 12:25:43 -0600

    completed shortlongshort

--- ShortLongShort.java
+++ ShortLongShort.java
@@ -1,5 +1,11 @@
 public class ShortLongShort {
     public static void main(String[] args) {
-        
+        String word1 = args[0];
+        String word2 = args[1];
+        if (word1.length() >= word2.length()) {
+            System.out.println(word2 + " | " + word1 + " | " + word2);
+        } else {
+            System.out.println(word1 + " | " + word2 + " | " + word1);
+        }
     }
 }
