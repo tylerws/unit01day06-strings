3e28034e0c556b67d7d5db6b87334a282a79a86f
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-24 11:24:17 -0600

    implemented abbreviate

--- Abbreviate.java
+++ Abbreviate.java
@@ -1,5 +1,12 @@
 public class Abbreviate {
     public static void main(String[] args) {
-        
+        String word = args[0];
+        if (word.length() < 8) {
+            System.out.println(word);
+        } else {
+            String abbr1 = word.substring(0, 2);
+            String abbr2 = word.substring(word.length() - 2, word.length());
+            System.out.println(abbr1 + "..." + abbr2);
+        }
     }
 }
