be981de8cee87cc614e990ef496429c27679d72e
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-31 19:41:26 -0600

    NegatePhrase.java feedback

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,3 +1,4 @@
+// TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result. Also, you never checked whether it already started with 'not', as discussed in the instructions
 public class NegatePhrase {
     public static void main(String[] args) {
         String sentence = args[0];
